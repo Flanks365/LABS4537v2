9c79677a36aa099158594057f5c5e686ee3eaa55
Flanks365 <ble16@my.bcit.ca>
2025-01-29 23:24:48 -0800

    Update index.js

--- COMP4537/labs/3/index.js
+++ COMP4537/labs/3/index.js
@@ -2,6 +2,7 @@
 const http = require('http');
 
 const { Lab3Utils } = require('./public/modules/utils'); 
+const {missing } = require('./public/lang/en/msg');
 
 const server = http.createServer((req, res) => {
 
@@ -10,6 +11,13 @@ const server = http.createServer((req, res) => {
     console.log(url);
     if(url.includes('/COMP4537/labs/3')){
         Lab3Utils.checkEndpoint(req,res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`<html>
+                    <body>
+                        <p>${missing.miss}</p>
+                    </body>
+                </html>`);
     }
 });
 
