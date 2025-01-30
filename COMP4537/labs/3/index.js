4f9b39af883ccf7b020464c0bbd4606372c8337d
Flanks365 <ble16@my.bcit.ca>
2025-01-29 23:09:53 -0800

    Updated and added 404

--- COMP4537/labs/3/index.js
+++ COMP4537/labs/3/index.js
@@ -2,6 +2,7 @@
 const http = require('http');
 
 const { Lab3Utils } = require('./public/modules/utils'); 
+const { miss } = require('./public/lang/en/msg');
 
 const server = http.createServer((req, res) => {
 
@@ -11,12 +12,13 @@ const server = http.createServer((req, res) => {
     if(url.includes('/COMP4537/labs/3')){
         Lab3Utils.checkEndpoint(req,res);
     }
-});
-
-const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0'; 
-
-server.listen(PORT,HOST, () => {
-    console.log(`Server running at http://${HOST}:${PORT}/`);
-});
+    else{
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(`<html>
+                    <body>
+                        <p>${miss.miss}</p>
+                    </body>
+                </html>`);
+    }
+}).listen(8080);
 
--- COMP4537/labs/3/public/lang/en/msg.js
+++ COMP4537/labs/3/public/lang/en/msg.js
@@ -1,5 +1,8 @@
 const msg = `Hello %s, what a beautiful day. Server time is %s`;
 
+const miss = '404 Not Found';
+
 module.exports = {
-    msg
+    msg,
+    miss
 };
