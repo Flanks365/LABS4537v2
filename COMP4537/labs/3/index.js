4e85f2d70f271a7ade5e31416ac5d9b9941803db
Flanks365 <ble16@my.bcit.ca>
2025-02-07 23:04:49 -0800

    Update index.js

--- COMP4537/labs/3/index.js
+++ COMP4537/labs/3/index.js
@@ -5,9 +5,7 @@ const { Lab3Utils } = require('./public/modules/utils');
 const Lab4Utils = require('./4/public/JS/app.js');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allows all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
 
 
     if (req.method === 'OPTIONS') {
