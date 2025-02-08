60a086ddf03eefa0754ba917e4cfc48812b25d3e
Flanks365 <ble16@my.bcit.ca>
2025-02-07 23:15:27 -0800

    Update app.js

--- COMP4537/labs/3/4/public/JS/app.js
+++ COMP4537/labs/3/4/public/JS/app.js
@@ -155,7 +155,7 @@ class dictionaryUtils{
 
                     !validStringRegex.test(newWord) || 
 
-                    !validStringRegex.test(newMeaning)) {
+                    !validStringRegex.test(newMeaning) || word.length === 0 || meaning.length === 0) {
 
                     res.writeHead(400, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({ error: 'Invalid input. Only non-empty alphabetic strings are allowed.' }));
