14275a11c5a66d3405ea9f448d143865a1ad60d3
Flanks365 <ble16@my.bcit.ca>
2025-03-25 15:39:11 -0700

    Added Cookie requirements

--- COMP4537/labs/3/index.js
+++ COMP4537/labs/3/index.js
@@ -13,6 +13,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allows all origins
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); 
 
     if (req.method === 'OPTIONS') {
         res.writeHead(204); // No content response
